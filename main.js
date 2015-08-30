893708692a64629053dbefaeda59e5aa803b25cf
finafisken <markus.warne@gmail.com>
2015-08-31 00:31:17 +0200

    animated svg

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var btn = document.getElementById("toggle-anim");
 var bg = document.getElementsByTagName("body")[0];
 
+
 btn.addEventListener("click", function(){
     if (btn.className.indexOf("paused") === -1){
         btn.classList.add("paused");
@@ -13,3 +14,13 @@ btn.addEventListener("click", function(){
         bg.style.animationPlayState = "running";
     }
 });
+console.log("Well hello there fellow interwebz adventurer!");
+console.log("If you find any bugs or quriks feel free to submit a pull request - https://github.com/finafisken/finafisken.github.io.git")
+console.log("Have a nice day!");
+
+
+
+// var a = document.getElementById("logo-svg");
+// var svgDoc = a.contentDocument;
+// var path = svgDoc.getElementById("o2");
+// var total_length = path.getTotalLength();
